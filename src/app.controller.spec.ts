0b61e3589a9c320e583d26647bdf26b66093d3e2
Starck Lin <zenyer@gmail.com>
2020-03-05 11:18:01 +0800

    fix test error

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RenderModule } from 'nest-next';
 // import { Res } from '@nestjs/common';
 // import { RenderableResponse } from 'nest-next';
 
@@ -9,15 +10,16 @@ describe('AppController', () => {
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
+      imports: [RenderModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
   });
 
   describe('getHello', () => {
-    it('should return "Hello World!"', () => {
+  it('should return "Hello World!"', () => {
       const appController = app.get<AppController>(AppController);
-    expect(appController.getHello()).toContain('Hello World!');
+      expect(appController.getHello().title).toContain('Hello World!');
     });
   });
 });
